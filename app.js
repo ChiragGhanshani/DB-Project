3a89f46c994d3dfc26510127df0cb934db190c3f
chirag <chiragghanshani@gmail.com>
2016-03-21 22:37:37 -0500

    altered rendering engine

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('html', cons.swig);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
