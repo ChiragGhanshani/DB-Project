f92b9ee2d79c12272e9d8c2ed0a9d2312f20aa1c
Josef <bob@bob.com>
2016-03-21 22:10:23 -0500

    changed things to categorical types, added comments next to potential multi val fields, removed some old comments, moved inserts into separate script

---
+++ db_inserts.sql
@@ -0,0 +1,75 @@
+
+INSERT INTO employee
+VALUES (000001,
+	'Clifford',
+	'The Dog',
+	'1234 Imaginary Ln.',
+	'Made Up Ville',
+	'ZZ',
+	12345,
+	'000-000-0000',
+	'nothing@nothing.com',
+	'123456789',
+	000001,
+	12,
+	'Mascot');
+	
+INSERT INTO habitats
+VALUES (0001, 000001, 'Cougar', 'Savannah');
+
+
+INSERT INTO species
+VALUES ('Ailurus fulgens', 
+	5,
+	'Mountain forest',
+	'Omnivore',
+	'2016-03-15 05:13:00',
+	'Solitary');
+	
+	INSERT INTO customers
+VALUES(
+	012345,
+	'Homer',
+	'J',
+	'Simpson',
+	'1960-05-14',
+	'thesimpsons@springfield.com',
+	'employee'
+);
+
+/*
+INSERT INTO transactions
+VALUES(
+	123,
+	123564364,
+	'pointless things',
+	50
+);
+*/
+
+/*
+INSERT INTO animals 
+VALUES(
+	7777777,
+	'm',
+	'angry',
+	'2001-06-11',
+	'ebola',
+	'1980-01-02',
+	'2010-03-05',
+	'2011-02-07',
+	'2015-11-30',
+	'O-',
+	'Homo Sapien'
+);
+*/
+
+INSERT INTO users
+VALUES(
+	'khoffee',
+	'abc123',
+	'2016-03-14',
+	'Y',
+	'ABD590',
+	'customer'
+);
