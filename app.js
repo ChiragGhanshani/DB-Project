dd782a06c91a1c2f65aca7a3f1ee6edeb40e76e9
achernan <achernandez5@uh.edu>
2016-03-22 00:05:26 -0500

    Started homepage for Zoo

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/static', express.static('public/images/'));
-app.use('/style', express.static('pubilc/stylesheets/'));
+app.use('/static', express.static('public/'));
 
 
 app.use('/', routes);
