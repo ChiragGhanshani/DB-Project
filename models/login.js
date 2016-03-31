ba9e8e92baf712de39bd5f8034b3acf02e940ea1
Josef0 <jmanlamell@gmail.com>
2016-03-31 00:12:10 -0500

    almost working first ORM code and canary test

--- models/login.js
+++ models/login.js
@@ -1,20 +1,29 @@
 var db = require('../models/db');
+var async = require('async');
 
 module.exports = {
 	getUserID : function(username, password, sendResultBack) {
 		if(typeof username === "undefined" || typeof password === "undefined" || username == "" || password == "")
 			sendResultBack(new Error('Invalid Credentials'), null);
+			//return 'no';
 		else
 		{
-			var queryString = 'SELECT user_id FROM users WHERE username = ? AND password = ?';
-		    var query = db.query(queryString, [username, password], function(err, rows, fields) {
+			var queryString = 'SELECT user_id FROM users WHERE username = \'' + username + '\' AND password = \''
+				+ password + '\';';
+			//return queryString;
+			//var query = db.query();
+		    var query = db.query(queryString, function(err, rows, fields) {
+				return "hmm?";
 			    if (err) {
-			      return sendResultBack(err, null);
+					return sendResultBack(err, null);
+			      //return err;
 			    }
 			    if (rows.length === 0) {
-			      sendResultBack(new Error('no such user'));
+					sendResultBack(new Error('No such user'));
+			      //return (new Error('no such user'));
 			    } else
-				  sendResultBack(null, rows[0].user_id);
+					sendResultBack(null,rows[0].username);
+				  //return 'please';
 		    });
 		}
 	}
--- test/canary.js
+++ test/canary.js
@@ -1,7 +1,18 @@
 var expect = require('chai').expect;
+var loginORM = require('../models/login.js');
 
 describe('canary test', function(){
   it('canary should pass', function(){
     expect(1).to.eql(1);
   });
 });
+
+describe('login ORM test', function(){
+  it('getUserID should return list', function(){
+    //console.log(LoginORM.getUserID('khoffee','abc123'));
+    console.log(loginORM.getUserID('khoffee','abc123',function(err, result){
+      return result;
+    }));
+    expect(1).to.eql(1);
+  })
+})
