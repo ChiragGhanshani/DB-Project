a04b72cc0edce397c9b8673c00b0c6c64dc78836
chirag <chiragghanshani@gmail.com>
2016-04-01 21:15:54 -0500

    Moved tests for login orm to separate file from the canary tests to maintain modularity

--- test/canary.js
+++ test/canary.js
@@ -7,13 +7,3 @@ describe('canary test', function(){
     expect(1).to.eql(1);
   });
 });
-
-describe('login ORM test', function(){
-  it('getUserID should return list', function(){
-    var parser = function(error, result){
-      expect(result[0].user_id).to.eql('ABD590');
-    }
-
-    loginORM.getUserID('khoffee', 'abc123', parser);
-  })
-})
