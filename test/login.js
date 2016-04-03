0eb619cf2179949e14e560de74f5e878cd889259
chirag <chiragghanshani@gmail.com>
2016-04-03 18:45:54 -0500

    fixed testing issue in which incorrect assertion did not fail

--- test/login.js
+++ test/login.js
@@ -3,9 +3,10 @@ var db = require('../models/db.js');
 var loginORM = require('../models/login.js');
 
 describe('login ORM test', function(){
-  it('getUserID should return list', function(){
+  it('getUserID should return list', function(done){
     var parser = function(error, result){
       expect(result[0].user_id).to.eql('ABD590');
+      done();
     }
 
     loginORM.getUserID('khoffee', 'abc123', parser);
