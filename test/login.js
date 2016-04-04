cf4740b620ab7f80fcce7f283dfa5683f2d77d8e
chirag <chiragghanshani@gmail.com>
2016-04-04 12:43:00 -0500

    Completed tests for login ORM

--- test/login.js
+++ test/login.js
@@ -3,7 +3,7 @@ var db = require('../models/db.js');
 var loginORM = require('../models/login.js');
 
 describe('login ORM test', function(){
-  it('getUserID should return list', function(done){
+  it('getUserID should return a userid', function(done){
     var parser = function(error, result){
       expect(result[0].user_id).to.eql('ABD590');
       done();
@@ -11,4 +11,22 @@ describe('login ORM test', function(){
 
     loginORM.getUserID('khoffee', 'abc123', parser);
   })
+
+  it('should return an error when an empty string is queried', function(done){
+    var parser = function(error, result){
+      expect(error).to.be.an('error');
+      done();
+    }
+
+    loginORM.getUserID('', '', parser);
+  });
+
+  it('should return an error when a non-existant entry is queried', function(done){
+    var parser = function(error, result){
+      expect(error).to.be.an('error');
+      done();
+    }
+
+    loginORM.getUserID('bapalabada', 'quintus', parser);
+  });
 });
