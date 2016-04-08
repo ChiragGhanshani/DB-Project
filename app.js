318acea7de6ba46d7c6156dad2f135a31330bfc5
Josef0 <jmanlamell@gmail.com>
2016-04-08 16:01:10 -0500

    created basic login page (not yet functional) and added one jpg to use as background

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var cons = require('consolidate');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var aboutPage = require('./routes/aboutPage');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use('/static', express.static('public/'));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/about', aboutPage);
+app.use('/login',login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
