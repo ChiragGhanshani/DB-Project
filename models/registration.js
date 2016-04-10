ab072140bf6562c96f4e7e373746cbd4c3383edd
Khoffee <khoipham7501@gmail.com>
2016-04-10 17:49:27 -0500

    Added registration.js

--- models/registration.js
+++ models/registration.js
@@ -11,4 +11,25 @@ module.exports = {
       }
     });
   }
+  insertCustomerUser : function(firstName, lastName, email, password, phoneNumber, email, dob, gender, streetAddress, zipCode, city, state, membershipType, sendResultBack){
+  	if(typeof firstName === "undefined" || typeof lastName === "undefined" || typeof email === "undefined"|| typeof password === "undefined" || typeof phoneNumber === "undefined" || typeof email === "undefined" || typeof dob === "undefined" || typeof gender === "undefined" || typeof streetAddress === "undefined" || typeof zipCode === "undefined" || typeof city === "undefined" || typeof state === "undefined" || typeof membershipType === "undefined")
+  		sendResultBack(new Error('Invalid data type'), null);
+  	else if( firstName == "" ||  lastName == "" ||  email == ""||  password == "" ||  phoneNumber == "" ||  email == "" ||  dob == "" ||  gender == "" ||  streetAddress == "" ||  zipCode == "" ||  city == "" || typeof state == "" || typeof membershipType == "")
+  		sendResultBack(new Error('Empty input'), null);
+  	else
+  	{
+  		var id = null;
+  		getUUID(function(err, result){
+  			id = result['UUID()'];
+  		});
+  		var createUUID = 'SET @';
+  		var insertString = 'INSERT INTO customers(membership_id, customer_firstName, customer_lastName, customer_DOB, customer_email, membership_type) VALUES (?, ?, ?, ?, ?, ?)';
+  		var insertion = db.query(insertString, [id, firstName, lastName, dob, email, membershipType], function(err, results){
+  			if(err)
+  				sendResultBack(new Error('Unable to insert data'), null);
+  			else
+  				sendResultBack(null, null);
+  		});
+  	}
+  }
 }
