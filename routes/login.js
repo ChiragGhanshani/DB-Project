71d75048ab5750f23c97f366f46fc6804232a31e
chirag <chiragghanshani@gmail.com>
2016-04-11 13:30:41 -0500

    created hook to make database calls

--- routes/login.js
+++ routes/login.js
@@ -3,10 +3,22 @@
  */
 var express = require('express');
 var router = express.Router();
+var db = require('../models/db');
+var loginORM = require('../models/login');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Login' });
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/checkCredentials', function(req, res, next){
+  var username = req.query.ID;
+  var password = req.query.password;
+
+  loginORM.getUserID(username, password, function(error, result){
+    if(error) res.send(error);
+    else res.send(result[0]);
+  });
 });
 
 module.exports = router;
