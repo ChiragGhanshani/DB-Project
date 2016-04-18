286205588310cd8da2b774789780782924101fd8
Ana <achernandez5@uh.edu>
2016-04-17 20:14:41 -0500

    Fixing merge issues

--- routes/memberPage.js
+++ routes/memberPage.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('members', { title: 'Membership' });
+    res.render('memberFAQ', { title: 'Membership FAQ' });
 });
 
 module.exports = router;
