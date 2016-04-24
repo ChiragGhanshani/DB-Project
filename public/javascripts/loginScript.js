f3f9191d6fd20a9d85b3ad6049c84085ee951a73
chirag <chiragghanshani@gmail.com>
2016-04-24 15:53:38 -0500

    placed all cookie management code into one file

--- public/javascripts/loginScript.js
+++ public/javascripts/loginScript.js
@@ -47,24 +47,6 @@ function checkLogin() {
     }
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-
-function setCookie(cookieName, value, expDays){
-    var d = new Date();
-    d.setTime(d.getTime() + Math.round(expDays * 24 * 60 * 60 * 1000));
-    var expires = 'expires='+d.toUTCString();
-    document.cookie = cookieName + "=" + JSON.stringify(value) + ';' + 'path=/;';
-}
-
 function addCustomer() {
     var valid = true;
     var address = document.getElementById('address').value;
--- public/javascripts/shoppingCartCookie.js
+++ public/javascripts/shoppingCartCookie.js
@@ -1,22 +1,3 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-
-function setCookie(cookieName, value, expDays){
-    var d = new Date();
-    d.setTime(d.getTime() + Math.round(expDays * 24 * 60 * 60 * 1000));
-    var expires = 'expires='+d.toUTCString();
-    document.cookie = cookieName + '=' + JSON.stringify(value) + ';' + 'path=/;' +
-        'name=' + cookieName + ';';
-}
-
 function addCartInfo() {
     //var cart = {'1':  0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0};
 
