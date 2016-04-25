bc42592ca8cd68c507f59a6f29aa16e1fc9ab1d1
khoffee <khoipham7501@gmail.com>
2016-04-25 18:20:34 -0500

    Added more sample data to db_inserts.sql

--- db_inserts.sql
+++ db_inserts.sql
@@ -60,12 +60,6 @@ VALUES
 	('anabanana', '123abc', '2016-04-20', '1234-7777-1234', 'Employee', 'Y'),
 	('ronak', 'hclc', '2016-01-01', '1234-7890-1234', 'Employee', 'Y');
 
-INSERT INTO areas
-VALUES(
-	01,
-	'Temperate Forest'
-);
-
 INSERT INTO employee_roles
 VALUES
 	(0, 'Keeper'),
@@ -96,82 +90,50 @@ VALUES
 SET @manager_id_variable = uuid();
 INSERT INTO employees
 VALUES
-	('1234-5555-1234', 'Clifford', 'The Dog', '1234 Imaginary Ln.', 'Made Up Ville',  /*state*/01, 12345, '000-000-0000', '1960-05-14', 'nothing@nothing.com', '123456789', @manager_id_variable, 1200, 0, 'Y'),
-	('1234-7777-1234', 'Ana', 'Banana', '4800 Calhoun Rd.', 'Houston', 42, 77004, '123-456-7777', '2001-01-01', 'ana@banana.com', '123777123', @manager_id_variable, 1500, 1, 'Y'),
+	('1234-5555-1234', 'Khoi', 'Pham', '1234 Imaginary Ln.', 'Made Up Ville',  /*state*/01, 12345, '000-000-0000', '1960-05-14', 'nothing@nothing.com', '123456789', @manager_id_variable, 1200, 0, 'Y'),
+	('1234-7777-1234', 'Ana', 'Citlalli', '4800 Calhoun Rd.', 'Houston', 42, 77004, '123-456-7777', '2001-01-01', 'ana@banana.com', '123777123', @manager_id_variable, 1500, 1, 'Y'),
 	('1234-7890-1234', 'Ronak', 'Shah', '123 Yolo Ln.', 'Houston', 42, 77072, '832-123-4567', '1969-04-20', 'ron@k.com', '123123123', @manager_id_variable, 2000, 2, 'Y');
 
 INSERT INTO customers
-VALUES(
-	'1234-1234-1234',
-	'Homer',
-	'Simpson',
-	'12420 Washington St.',
-	'Houston',
-	42,
-	77072,
-	'832-348-3887',
-	'1960-05-14',
-	'thesimpsons@springfield.com',
-	'Y'
-);
+VALUES
+	('1234-1234-1234', 'Homer', 'Simpson', '12420 Washington St.', 'Houston', 42, 77072, '832-348-3887', '1960-05-14', 'thesimpsons@springfield.com', 'Y');
 
 SET @transaction_id_variable = uuid();
 INSERT INTO transactions
-VALUES(
-	@transaction_id_variable,
-	'1234-1234-1234',
-	'2016-01-02 00:00:00.000'
-);
+VALUES
+	(@transaction_id_variable, '1234-1234-1234', '2016-01-02 00:00:00.000');
 
 INSERT INTO transaction_items
-VALUES(
-	@transaction_id_variable,
-	01,
-	1
-);
+VALUES
+	(@transaction_id_variable, 01, 1);
 
 INSERT INTO areas(id, area)
 VALUES
 	(0, 'Rainforest'),
-	(8, 'Savanna'),
-	(2, 'Desert'),
-	(3, 'Grasslands'),
-	(4, 'Arctic'),
-	(5, 'Saltwater'),
-	(6, 'Freshwater'),
-	(7, 'Jungle');
+	(1, 'Temperate Forest'),
+	(2, 'Savanna'),
+	(3, 'Desert'),
+	(4, 'Grasslands'),
+	(5, 'Arctic'),
+	(6, 'Saltwater'),
+	(7, 'Freshwater'),
+	(8, 'Jungle');
 
 INSERT INTO species
-VALUES(
-	'Ailurus fulgens',
-	5,
-	'Mountain forest',
-	'Herbivore',
-	01 /*behavior*/
-);
+VALUES
+	('Ailurus fulgens', 5, 'Mountain forest', 'Herbivore', 4),
+	('Panthera leo', 420, 'Grasslands', 'Omnivore', 1),
+	('Phoenicopterus chilensis', 3, 'Lagoons', 'Herbivore', 5),
+	('Tyto Alba', 2, 'Rainforest', 'Omnivore', 3);
 
 INSERT INTO habitats
-VALUES(
-	1,
-	'1234-5555-1234',
-	'2016-03-15 05:13:00',
-	01 /*area*/
-);
+VALUES
+	(1, '1234-5555-1234', '2016-03-15 05:13:00', 01);
 
 SET @tag_number_variable = uuid();
 INSERT INTO animals
-VALUES(
-	@tag_number_variable,
-	'Female',
-	'2001-06-11',
-	'1980-01-02',
-	'2010-03-05',
-	'2011-02-07',
-	'2015-11-30',
-	'A+',
-	'Ailurus fulgens',
-	1
-);
+VALUES
+	(@tag_number_variable, 'Female', '2001-06-11', '1980-01-02', '2010-03-05', '2011-02-07', '2015-11-30', 'A+', 'Ailurus fulgens', 1);
 
 INSERT INTO animal_illnesses
 VALUES
@@ -179,22 +141,9 @@ VALUES
 	(@tag_number_variable, 'rAIDs', 'Acquisition of red cubes by the animal\'s body. Upon acquiring three, the animal spreads it to the other animals in its exhibit'),
 	(@tag_number_variable, 'blAIDs', 'Acquisition of black cubes by the animal\'s body. Upon acquiring three, the animal spreads it to the other animals in its exhibit'),
 	(@tag_number_variable, 'yAIDs', 'Acquisition of yellow cubes by the animal\'s body. Upon acquiring three, the animal spreads it to the other animals in its exhibit'),
-	(@tag_number_variable, 'AIDS', 'This red panda was perhaps a little too promiscuous in her youth...');
-
-INSERT INTO animal_illnesses
-VALUES(
-	@tag_number_variable,
-	'Flu',
-	'She has been sniffling for days!'
-);
+	(@tag_number_variable, 'AIDS', 'This red panda was perhaps a little too promiscuous in her youth...'),
+	(@tag_number_variable, 'Flu', 'She has been sniffling for days!');
 
 INSERT INTO zoo_events
-VALUES(
-	1,
-	'2016-04-11',
-	'Class Presentation',
-	'17:30:00',
-	'19:00:00',
-	null,
-	'So soon...'
-);
+VALUES
+	(1, '2016-04-11', 'Class Presentation', '17:30:00', '19:00:00', null, 'So soon...');
