204817463d36216cde7d108075715fd3dcf8efef
Josef0 <jmanlamell@gmail.com>
2016-04-24 21:45:05 -0500

    Added a few simple ORM functions to allow basic queries on Customers and Animals tables which can then be processed on front-end. Added basic section comments.

--- models/reports.js
+++ models/reports.js
@@ -2,6 +2,8 @@ var db = require('./db.js');
 var async = require('async');
 
 module.exports = {
+  /****transaction reporting****/
+
   getTransactionsReportNoFilter : function(sendResultBack) {
     var queryString = 'SELECT TRAN.transaction_id, IT.item_type, IT.id, IT.item_cost, TRAN_ITEM.quantity, TRAN.transaction_time FROM transactions AS TRAN, item_types AS IT, transaction_items AS TRAN_ITEM;';
     db.query(queryString).spread(function(rows) {
@@ -34,6 +36,10 @@ module.exports = {
       });
     }
   },
+
+  /****illness reporting****/
+
+
   getIllnessesReportNoFilter : function(sendResultBack) {
       var queryString = 'SELECT * FROM animal_illnesses;';
       db.query(queryString).spread(function(rows) {
@@ -66,6 +72,10 @@ module.exports = {
       });
     }
   },
+
+  /****employee reporting****/
+
+
   getEmployeesReportNoFilter : function(sendResultBack) {
     var queryString = 'SELECT employee_id AS "Employee ID", employee_firstname AS "First Name", employee_lastname AS "Last Name", salary, manager_id AS "Manager ID" FROM employees;';
     db.query(queryString).spread(function(rows) {
@@ -91,6 +101,76 @@ module.exports = {
       sendResultBack(new Error('Invalid filter'), null);
     else {
       var queryString = 'SELECT employee_id AS "Employee ID", employee_firstname AS "First Name", employee_lastname AS "Last Name", salary, manager_id AS "Manager ID" FROM employees WHERE ? = ? AND ? = ?;';
+      db.query(queryString, [column, value]).spread(function(rows) {
+        if(rows.length > 0) {
+          sendResultBack(null, rows);
+        }
+      });
+    }
+  },
+
+  /****customer reporting****/
+
+  getCustomersReportNoFilter : function(sendResultBack) {
+    var queryString = 'SELECT * FROM customers;'; //I feel like we can process this on client side rather than limiting the data returned arbitrarily from this side...
+    db.query(queryString).spread(function(rows) {
+      if(rows.length > 0) {
+        sendResultBack(null, rows);
+      }
+    });
+  },
+  getCustomersReportOneFilter : function(column ,value, sendResultBack) {
+    if([typeof column, typeof value].indexOf("undefined") >= 0)
+      sendResultBack(new Error('Invalid filter'), null);
+    else {
+      var queryString = 'SELECT * FROM customers WHERE ? = ?;';
+      db.query(queryString, [column, value]).spread(function(rows) {
+        if(rows.length > 0) {
+          sendResultBack(null, rows);
+        }
+      });
+    }
+  },
+  getCustomersReportTwoFilters : function(column1, value1, column2, value2, sendResultBack) {
+    if([typeof column1, typeof value1, typeof column2, typeof value2].indexOf("undefined") >= 0)
+      sendResultBack(new Error('Invalid filter'), null);
+    else {
+      var queryString = 'SELECT * FROM customers WHERE ? = ? AND ? = ?;';
+      db.query(queryString, [column, value]).spread(function(rows) {
+        if(rows.length > 0) {
+          sendResultBack(null, rows);
+        }
+      });
+    }
+  },
+
+  /****animal reporting****/
+
+  getAnimalsReportNoFilter : function(sendResultBack) {
+    var queryString = 'SELECT * FROM animals;'; //I feel like we can process this on client side rather than limiting the data returned arbitrarily from this side...
+    db.query(queryString).spread(function(rows) {
+      if(rows.length > 0) {
+        sendResultBack(null, rows);
+      }
+    });
+  },
+  getAnimalsReportOneFilter : function(column ,value, sendResultBack) {
+    if([typeof column, typeof value].indexOf("undefined") >= 0)
+      sendResultBack(new Error('Invalid filter'), null);
+    else {
+      var queryString = 'SELECT * FROM animals WHERE ? = ?;';
+      db.query(queryString, [column, value]).spread(function(rows) {
+        if(rows.length > 0) {
+          sendResultBack(null, rows);
+        }
+      });
+    }
+  },
+  getAnimalsReportTwoFilters : function(column1, value1, column2, value2, sendResultBack) {
+    if([typeof column1, typeof value1, typeof column2, typeof value2].indexOf("undefined") >= 0)
+      sendResultBack(new Error('Invalid filter'), null);
+    else {
+      var queryString = 'SELECT * FROM animals WHERE ? = ? AND ? = ?;';
       db.query(queryString, [column, value]).spread(function(rows) {
         if(rows.length > 0) {
           sendResultBack(null, rows);
