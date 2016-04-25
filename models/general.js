6f86e99aaf941ea528fd5a5403edd16fa1e97d1c
Khoffee <khoipham7501@gmail.com>
2016-04-24 23:19:42 -0500

    Added general ORM

---
+++ models/general.js
@@ -0,0 +1,22 @@
+var db = require('./db.js');
+var async = require('async');
+
+module.exports = {
+
+  selectTable : function(table, sendResultBack) {
+    var queryString = 'SELECT * FROM ?;';
+    db.query(queryString, table).spread(function(rows) {
+      if(rows.length > 0) {
+        sendResultBack(null, rows);
+      }
+    });
+  },
+  updateTable : function(table, column, value, whereColumn, whereValue, sendResultBack) {
+    var queryString = 'UPDATE ? SET ?=? WHERE ?=?;';
+    db.query(queryString,[table, column, value, whereColumn, whereValue]).then(function() {
+      sendResultBack(null, null);
+    }).catch(function(error) {
+      sendResultBack(error, null);
+    });
+  }
+}
